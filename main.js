9835c9fb3b6a68143554aab3573498b5b7292536
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-10 16:18:11 +0100

    sv

--- main.js
+++ main.js
@@ -7,12 +7,48 @@ const membri = [
         immagine: "wayne-barnett-founder-ceo.jpg",
         
 
-    }
+    },
+    {
+        nome: "Angela Caroll",
+        ruorlo: "Chief Editor",
+        immagine: "angela-caroll-chief-editor.jpg",
+        
+
+    },
+    {
+        nome: "Walter Gordon",
+        ruorlo: "Office Manager",
+        immagine: "walter-gordon-office-manager.jpg",
+        
+
+    },
+    {
+        nome: "Angela Lopez",
+        ruorlo: "Social Media Manager",
+        immagine: "angela-lopez-social-media-manager.jpg",
+        
+
+    },
+    {
+        nome: "Scott Estrada",
+        ruorlo: "Developer",
+        immagine: "scott-estrada-developer.jpg",
+        
+
+    },
+    {
+        nome: "Barbara Ramos",
+        ruorlo: "Graphic Designer",
+        immagine: "barbara-ramos-graphic-designer.jpg",
+        
+
+    },
+    
 ]
 
 for (let i = 0; i < membri.length; i++) {
     
-   console.log(`Nome: ${membri[i].nome} Ruolo: ${membri[i].ruorlo} Eta: ${membri[i].immagine}`);
+   console.log(` ${membri[i].nome}  ${membri[i].ruorlo} ${membri[i].immagine}`);
     
     
 
